11476abcda847763771329e0d3e000338754ba0f
axelbriandip <axelbriandip.rg@gmail.com>
2023-09-14 09:25:19 -0300

    form processed and validated

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.urls import reverse # Funcionaría como el template tag 'url'
 
 # Create your views here.
 def contact(request):
     contact_form = ContactForm()
+
+    if request.method == 'POST':
+        contact_form = ContactForm(data = request.POST)
+        if contact_form.is_valid():
+            name = request.POST.get('name','')
+            email = request.POST.get('email','')
+            content = request.POST.get('content','')
+            # Si todo fue bien, redireccionamos..
+            return redirect(reverse('contact')+'?ok')
+
     return render(request, 'contact/contact.html', {'form':contact_form})
